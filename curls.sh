4ecf9e6556a699749b1426c716b36d953615eed3
Pedro Schneider <correaschneider@gmail.com>
2025-03-28 15:25:50 -0300

    Removing old curls

--- curls.sh
+++ curls.sh
@@ -1,37 +1,5 @@
 #! /bin/bash
 
-MODEL_ID=$(curl -X POST http://backend.info.localhost/api/models \
-  -H "Content-Type: application/json" \
-  -d '{
-    "name": "Model Name"
-  }' | jq -r '.id')
-
-curl -X GET http://backend.info.localhost/api/models
-
-curl -X GET http://backend.info.localhost/api/models/${MODEL_ID}
-
-curl -X PUT http://backend.info.localhost/api/models/${MODEL_ID} \
-  -H "Content-Type: application/json" \
-  -d '{
-    "name": "Updated Model Name"
-  }'
-
-BRAND_ID=$(curl -X POST http://backend.info.localhost/api/brands \
-  -H "Content-Type: application/json" \
-  -d '{
-    "name": "Brand Name"
-  }' | jq -r '.id')
-
-curl -X GET http://backend.info.localhost/api/brands
-
-curl -X GET http://backend.info.localhost/api/brands/${BRAND_ID}
-
-curl -X PUT http://backend.info.localhost/api/brands/${BRAND_ID} \
-  -H "Content-Type: application/json" \
-  -d '{
-    "name": "Updated Brand Name"
-  }'
-
 VEHICLE_ID=$(curl -X POST http://backend.info.localhost/api/vehicles \
   -H "Content-Type: application/json" \
   -d '{
@@ -39,8 +7,8 @@ VEHICLE_ID=$(curl -X POST http://backend.info.localhost/api/vehicles \
     "year": 2020,
     "chassi": "12345678901234567",
     "renavam": "12345678901",
-    "modelId": "'${MODEL_ID}'",
-    "brandId": "'${BRAND_ID}'"
+    "model": "Modelo",
+    "brand": "Marca"
   }' | jq -r '.id')
 
 curl -X GET http://backend.info.localhost/api/vehicles
@@ -51,8 +19,8 @@ curl -X PUT http://backend.info.localhost/api/vehicles/${VEHICLE_ID} \
   -H "Content-Type: application/json" \
   -d '{
     "plate": "ABC1235",
-    "modelId": "'${MODEL_ID}'",
-    "brandId": "'${BRAND_ID}'"
+    "model": "Modelo 1",
+    "brand": "Marca 1"
   }'
 
 curl -X DELETE http://backend.info.localhost/api/vehicles/${VEHICLE_ID}
